59cfb8f146f00c53908d0216ef876560ffa1341e
Bruno Laia <100207161+brunolaia@users.noreply.github.com>
2026-06-08 15:29:12 -0300

    Update eng_homologa.py

--- eng_homologa.py
+++ eng_homologa.py
@@ -210,12 +210,22 @@ for linha in range(0, len(meses_com_dados), 3):
                 customdata=semana_df["Registros"]
             )
 
-            fig.update_layout(
-                title={"text": f"📅 {mes}", "x": 0.5},
-                height=320,
-                showlegend=False,
-                hovermode="x"
-            )
+            
+fig.update_traces(
+    hovertemplate="<b>%{x}</b><br>Quantidade: %{y}<br><br>%{customdata}<extra></extra>",
+    customdata=semana_df["Registros"],
+    hoverlabel=dict(
+        align="left"
+    )
+)
+
+fig.update_layout(
+    title={"text": f"📅 {mes}", "x": 0.5},
+    height=320,
+    showlegend=False,
+    hovermode="closest"
+)
+
 
             st.plotly_chart(fig, use_container_width=True)
 
