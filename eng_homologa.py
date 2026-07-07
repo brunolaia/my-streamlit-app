1796f0259f3c30362aef09cc1657ba46d6bd78f1
Bruno Laia <100207161+brunolaia@users.noreply.github.com>
2026-07-07 17:04:20 -0300

    Update eng_homologa.py

--- eng_homologa.py
+++ eng_homologa.py
@@ -269,10 +269,14 @@ for linha in range(0, len(meses_com_dados), 3):
 
           df_mes = df_filtro[df_filtro["Mês"] == mes]
 
-            semana_df = df_mes.groupby("Semana").agg(
-                Quantidade=("Registro", "count"),
-                Registros=("Registro", lambda x: "<br>".join(map(str, x)))
-            ).reset_index()
+            for idx, mes in enumerate(meses_com_dados[linha:linha + 3]):
+
+    with colsdf_mes = df_filtro[df_filtro["Mês"] == mes]
+
+        semana_df = df_mes.groupby("Semana").agg(
+            Quantidade=("Registro", "count"),
+            Registros=("Registro", lambda x: "<br>".join(map(str, x)))
+        ).reset_index()
 
 
             semana_df["SemanaNum"] = pd.to_numeric(
